c7a011bbeb5e0fb2e278e778bd9a271e38c6a9b0
Ricardo Signes <rjbs@cpan.org>
2010-09-23 16:01:27 -0400

    walkthrough comments for the tests

--- lib/Test/Routine/Runner.pm
+++ lib/Test/Routine/Runner.pm
@@ -5,7 +5,7 @@ package Test::Routine::Runner;
 use Carp qw(confess);
 use Class::MOP ();
 use Moose::Meta::Class;
-use Scalar::Util qw(blessed);
+use Scalar::Util qw(blessed reftype);
 use Test::More ();
 
 use Sub::Exporter::Util qw(curry_method);
@@ -31,21 +31,27 @@ sub _obj {
   $class->name->new($arg);
 }
 
+our $UPLEVEL = 0;
+
 sub run_tests {
   my ($self, $desc, $inv, $arg) = @_;
 
-  my @caller = caller;
-
-  $desc //= sprintf 'tests from %s, line %s', $caller[1], $caller[2];
-  $inv  //= $caller[0];
-
+  local $UPLEVEL = 1;
   _run_tests($desc, $inv, $arg);
 }
 
 sub _run_tests {
+  # Too much magic, we just want different run_tests-like routines, I think.
+  # -- rjbs, 2010-09-23
+  if (@_ == 1 and ! blessed $_[0]) {
+    if ((reftype $_[0]  // '') eq 'HASH') {
+      @_ = (undef, undef, $_[0]);
+    }
+  }
+
   my ($desc, $inv, $arg) = @_;
 
-  my @caller = caller;
+  my @caller = caller($UPLEVEL);
 
   $desc //= sprintf 'tests from %s, line %s', $caller[1], $caller[2];
   $inv  //= $caller[0];
@@ -65,10 +71,12 @@ sub _run_tests {
   my @tests = grep { $_->isa('Test::Routine::Test') }
               $thing->meta->get_all_methods;
 
-  for my $test (sort { $a->name cmp $b->name } @tests) {
-    my $name = $test->name;
-    Test::More::subtest($desc, sub { $thing->$name });
-  }
+  Test::More::subtest($desc, sub {
+    for my $test (sort { $a->name cmp $b->name } @tests) {
+      my $name = $test->name;
+      Test::More::subtest($name, sub { $thing->$name });
+    }
+  });
 }
 
 1;
