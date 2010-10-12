a3e9720fa47d971ee8d6d66a87aacbcf348abd45
Ricardo Signes <rjbs@cpan.org>
2010-10-12 10:50:22 -0400

    divide class and instance creation

--- lib/Test/Routine/Runner.pm
+++ lib/Test/Routine/Runner.pm
@@ -1,6 +1,5 @@
-use strict;
-use warnings;
 package Test::Routine::Runner;
+use Moose;
 # ABSTRACT: tools for running Test::Routine tests
 
 =head1 OVERVIEW
@@ -8,11 +7,11 @@ package Test::Routine::Runner;
 Test::Routine::Runner is documented in L<the Test::Routine docs on running
 tests|Test::Routine/Running Tests>.  Please consult those for more information.
 
-Both C<run_tests> and C<run_me> are methods on Test::Routine::Runner, and
-are exported by default with the invocant curried.  This means that you can
-write a subclass of Test::Routine::Runner with different behavior.  Do this
-cautiously.  Although the basic behavior of the runner are unlikely to change,
-they are not yet set entirely in stone.
+Both C<run_tests> and C<run_me> are methods on Test::Routine::Runner, and are
+exported by default with the invocant curried.  This means that you can write a
+subclass of Test::Routine::Runner with different behavior.  Do this cautiously.
+Although the basic behaviors of the runner are unlikely to change, they are not
+yet set entirely in stone.
 
 =cut
 
@@ -61,15 +60,11 @@ sub run_me {
   $class->run_tests($desc, $caller, $arg);
 }
 
-sub _invocant_for {
-  my ($class, $inv, $arg) = @_;
+sub _class_for {
+  my ($class, $inv) = @_;
 
-  confess "can't supply object and args for running tests"
-    if blessed $inv and $arg;
-
-  $arg = {} unless defined $arg;
-
-  return $inv if blessed $inv;
+  confess "can't supply preconstructed object for test class construction"
+    if blessed $inv;
 
   $inv = [ $inv ] if Params::Util::_CLASS($inv);
 
@@ -93,7 +88,18 @@ sub _invocant_for {
     cache        => 1,
     (@roles ? (roles => \@roles) : ()),
   );
+}
 
+sub _invocant_for {
+  my ($self, $thing, $arg) = @_;
+
+  confess "can't supply preconstructed object for running tests"
+    if $arg and blessed $thing;
+
+  return $thing if blessed $thing;
+
+  $arg ||= {};
+  my $new_class = $self->_class_for($thing);
   $new_class->name->new($arg);
 }
 
