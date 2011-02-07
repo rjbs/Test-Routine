bc8c6898ba4fb76512d88d9d68d782e727620cdc
Ricardo Signes <rjbs@cpan.org>
2011-02-06 21:24:20 -0500

    oops, let that ... operator slip in again

--- t/lib/NoGood.pm
+++ t/lib/NoGood.pm
@@ -1,8 +1,8 @@
 package t::lib::NoGood;
 use Test::Routine;
 
-test "this will be duplicated" => sub { ... };
+test "this will be duplicated" => sub { die 'Unimplemented' };
 
-test "this will be duplicated" => sub { ... };
+test "this will be duplicated" => sub { die 'Unimplemented' };
 
 1;
