6740a1f62463a9a464e0af10f800989dc345352b
Ricardo Signes <rjbs@cpan.org>
2014-09-01 21:51:35 -0400

    catch non-method sub name conflicts in caller
    see https://github.com/rjbs/Test-Routine/issues/7

--- lib/Test/Routine.pm
+++ lib/Test/Routine.pm
@@ -289,6 +289,9 @@ sub test {
   Carp::croak "can't have two tests with the same name ($name)"
     if $class->get_method($name);
 
+  Carp::croak "there's already a subroutine named $name in $caller"
+    if $caller->can($name);
+
   Carp::croak "can't name a test after a Moose::Object method ($name)"
     if Moose::Object->can($name);
 
